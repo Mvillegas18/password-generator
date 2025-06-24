4f628e63ed5b0eab256348b58a957ee006c88fdf
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-06-23 21:08:17 -0500

    feat: add generatePassword function for customizable password generation

---
+++ src/lib/password.ts
@@ -0,0 +1,37 @@
+interface Props {
+	hasLowercase?: boolean;
+	hasUppercase?: boolean;
+	hasNumbers?: boolean;
+	hasSymbols?: boolean;
+	length?: number;
+}
+
+export const generatePassword = ({
+	hasLowercase = true,
+	hasUppercase = false,
+	hasNumbers = false,
+	hasSymbols = false,
+	length = 12,
+}: Props): string => {
+	const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+	const numberChars = '0123456789';
+	const symbolChars = '!@#$%^&*()_+-=[]{}|;:,.<>?';
+
+	let charPool = '';
+
+	if (hasLowercase) charPool += lowercaseChars;
+	if (hasUppercase) charPool += uppercaseChars;
+	if (hasNumbers) charPool += numberChars;
+	if (hasSymbols) charPool += symbolChars;
+	if (charPool.length === 0) {
+		charPool = lowercaseChars; // Default to lowercase if no options are selected
+	}
+
+	let password = '';
+	for (let i = 0; i < length; i++) {
+		const randomIndex = Math.floor(Math.random() * charPool.length);
+		password += charPool[randomIndex];
+	}
+	return password;
+};
