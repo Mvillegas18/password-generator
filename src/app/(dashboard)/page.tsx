1ddca755a7a8a458e0f4502af2cf4f202e6e720d
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-06-25 19:50:26 -0500

    feat: add PasswordList component to Dashboard for displaying saved passwords

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,9 +1,11 @@
 import { FormCreatePassword } from './_components/form-create-password';
+import PasswordList from './_components/password-list';
 
 export default function Dashboard() {
 	return (
 		<div className="container mx-auto px-4">
 			<FormCreatePassword />
+			<PasswordList />
 		</div>
 	);
 }
