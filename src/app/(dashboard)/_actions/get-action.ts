92ec689e9e3db374807ba08f808ee7f9288e1236
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-06-30 19:07:31 -0500

    fix: add type annotation for passwords in getPasswordsAction

--- src/app/(dashboard)/_actions/get-action.ts
+++ src/app/(dashboard)/_actions/get-action.ts
@@ -2,9 +2,10 @@
 
 import { cryptr } from '@/lib/crypto';
 import prisma from '@/lib/prisma';
+import { Password } from '../../../../generated/prisma';
 
 export const getPasswordsAction = async () => {
-	const passwords = await prisma.password.findMany();
+	const passwords: Password[] = await prisma.password.findMany();
 	return passwords.map((password) => ({
 		...password,
 		decryptedPassword: cryptr.decrypt(password.encryptedPassword), // Assuming you want to return the encrypted password
